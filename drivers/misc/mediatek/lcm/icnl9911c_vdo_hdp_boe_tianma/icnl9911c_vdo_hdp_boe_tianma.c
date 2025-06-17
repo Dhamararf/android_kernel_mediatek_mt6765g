TelegramAt25/niigo_kernel_xiaomi_blossom

drivers/misc: mediatek: lcm: clean-up and add some new lcms
Add lcms

drivers/misc/mediatek/lcm/icnl9911c_vdo_hdp_boe_tianma/icnl9911c_vdo_hdp_boe_tianma.c
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2015 MediaTek Inc.
  * Copyright (C) 2021 XiaoMi, Inc.
+ * Copyright (C) 2022 TheKit
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -107,6 +108,7 @@ static const unsigned char LCD_MODULE_ID = 0x01;
 #define REGFLAG_RESET_HIGH	0xFFFF
 
 static struct LCM_DSI_MODE_SWITCH_CMD lcm_switch_mode_cmd;
+extern char *saved_command_line;
 
 #ifndef TRUE
 #define TRUE 1
@@ -116,6 +118,9 @@ static struct LCM_DSI_MODE_SWITCH_CMD lcm_switch_mode_cmd;
 #define FALSE 0
 #endif
 
+#if defined(CONFIG_TOUCHSCREEN_COMMON)
+extern bool tpd_gesture_flag;
+#endif
 struct LCM_setting_table {
 	unsigned int cmd;
 	unsigned char count;
@@ -123,52 +128,44 @@ struct LCM_setting_table {
 };
 
 static struct LCM_setting_table lcm_suspend_setting[] = {
-	{0x28, 0, {} },
-	{REGFLAG_DELAY, 2, {} },
-	{0x10, 0, {} },
-	{REGFLAG_DELAY, 145, {} },
+	{0xF0, 2, {0x5A, 0x59}},
+	{0xF1, 2, {0xA5, 0xA6}},
+	{0x26, 1, {0x08}},
+	{REGFLAG_DELAY, 1, {}},
+	{0x28, 1, {}},
+	{REGFLAG_DELAY, 20, {}},
+	{0x10, 1, {}},
+	{REGFLAG_DELAY, 110, {}},
+	{0xCC, 1, {0x01}},
+	{REGFLAG_DELAY, 10, {}},
+	{REGFLAG_END_OF_TABLE, 0, {}},
 };
 
+#if defined(CONFIG_TOUCHSCREEN_COMMON)
+static struct LCM_setting_table lcm_suspend_gesture_setting[] = {
+	{0x26, 1, {0x08}},
+	{REGFLAG_DELAY, 1, {}},
+	{0x28, 0, {}},
+	{REGFLAG_DELAY, 2, {}},
+	{0x10, 0, {}},
+	{REGFLAG_DELAY, 145, {}},
+	{REGFLAG_END_OF_TABLE, 0, {}},
+};
+#endif
+
 static struct LCM_setting_table init_setting[] = {
-#ifdef CONFIG_BACKLIGHT_SUPPORT_2047_FEATURE
-	{0xFF,0x01,{0x23}},
-	{REGFLAG_DELAY,1,{}},
-	{0xFB,0x01,{0x01}},
-	{0x00,0x01,{0x68}},//11bit
-	//PWM frequency 30K start
-	{0x07,0x01,{0x00}},
-	{0x08,0x01,{0x01}},
-	{0x09,0x01,{0x00}},
-	//PWM frequency 30K end
-#else
-	{0xFF,0x01,{0x23}},
-	{REGFLAG_DELAY,1,{}},
-	{0xFB,0x01,{0x01}},
-	{0x07,0x01,{0x20}},
-	{0x08,0x01,{0x07}},
-	{0x09,0x01,{0x02}},
-#endif
-
-	{0xFF,0x01,{0x10}},
-	{0xFB,0x01,{0x01}},
-	//Set_tear_on
-	{0x35,0x01,{0x00}},
-#ifdef CONFIG_BACKLIGHT_SUPPORT_2047_FEATURE
-	{0x51,0x02,{0x00,0x00}},
-#else
-	{0x51,0x01,{0x00}},
-#endif
-	{0x53,1,{0x2C}},
-	{0x55,1,{0x00}},
-	{0x68,2,{0x02,0x01}},
-	//Display_on
-	{0x29,0,{}},
-	//Sleep_out
-	{0x11,0,{}},
-	{REGFLAG_DELAY,100,{}},
-
-
-	/* {0x51,1,{0xFF}},     //      write   display brightness */
+	{0xF0, 2, {0x5A, 0x59}},
+	{0xF1, 2, {0xA5, 0xA6}},
+	{0xC1, 19, {0xC0, 0x0C, 0x20, 0x7C, 0x04, 0x50, 0x50, 0x04, 0x2A, 0x40, 0x36, 0x00, 0x07, 0xCF, 0xFF, 0xFF, 0x9E, 0x01, 0xC0}},
+	{0xE0, 13, {0x30, 0x00, 0x20, 0x18, 0x01, 0x1F, 0x23, 0x62, 0xDF, 0xA0, 0x04, 0xCC, 0x01}},
+	{0x35, 1, {0x00}},
+	{0x51, 2, {0x00, 0x00}},
+	{0x53, 1, {0x24}},
+	{0x11, 0, {}},
+	{REGFLAG_DELAY, 100, {}},
+	{0x29, 0, {}},
+	{REGFLAG_DELAY, 100, {}},
+	{0x26, 1, {0x01}},
 };
 
 #if 0
@@ -202,11 +199,7 @@ static struct LCM_setting_table lcm_deep_sleep_mode_in_setting[] = {
 };
 #endif
 static struct LCM_setting_table bl_level[] = {
-#ifdef CONFIG_BACKLIGHT_SUPPORT_2047_FEATURE
-	{0x51,2,{0x05,0xc2}},
-#else
-	{0x51,1,{0xB8}},
-#endif
+	{0x51, 2, {0xC2, 0x05}},
 	{REGFLAG_END_OF_TABLE, 0x00, {} }
 };
 
@@ -290,14 +283,14 @@ static void lcm_get_params(struct LCM_PARAMS *params)
 
 	params->dsi.PS = LCM_PACKED_PS_24BIT_RGB888;
 
-	params->dsi.vertical_sync_active = 2;
-	params->dsi.vertical_backporch = 8;
-	params->dsi.vertical_frontporch = 10;
+	params->dsi.vertical_sync_active = 4;
+	params->dsi.vertical_backporch = 18;
+	params->dsi.vertical_frontporch = 32;
 	params->dsi.vertical_active_line = FRAME_HEIGHT;
 
 	params->dsi.horizontal_sync_active = 4;
-	params->dsi.horizontal_backporch = 85;
-	params->dsi.horizontal_frontporch = 85;
+	params->dsi.horizontal_backporch = 80;
+	params->dsi.horizontal_frontporch = 80;
 	params->dsi.horizontal_active_pixel = FRAME_WIDTH;
 	params->dsi.ssc_disable = 1;
 #ifndef CONFIG_FPGA_EARLY_PORTING
@@ -315,7 +308,6 @@ static void lcm_get_params(struct LCM_PARAMS *params)
 	params->dsi.lcm_esd_check_table[0].cmd = 0x0A;
 	params->dsi.lcm_esd_check_table[0].count = 1;
 	params->dsi.lcm_esd_check_table[0].para_list[0] = 0x9C;
-
 }
 
 static void lcm_init_power(void)
@@ -324,10 +316,13 @@ static void lcm_init_power(void)
 	lcd_bl_en = 1;
 }
 
-
 static void lcm_suspend_power(void)
 {
 	no_printk("[LCM]%s\n",__func__);
+
+	if (tpd_gesture_flag) {
+		return;
+	}
 
 	disp_dts_gpio_select_state(DTS_GPIO_STATE_LCD_BIAS_ENN0);
 	MDELAY(2);
@@ -338,9 +333,6 @@ static void lcm_suspend_power(void)
 static void lcm_resume_power(void)
 {
 	no_printk("[LCM]%s\n",__func__);
-	disp_dts_gpio_select_state(DTS_GPIO_STATE_LCD_BIAS_ENP1);
-	
-	disp_dts_gpio_select_state(DTS_GPIO_STATE_LCD_BIAS_ENN1);
 }
 
 static void lcm_init(void)
@@ -348,6 +340,9 @@ static void lcm_init(void)
 	unsigned char cmd = 0x0;
 	unsigned char data = 0xFF;
 	int ret = 0;
+
+	no_printk("[LCM]%s\n",__func__);
+
 	disp_dts_gpio_select_state(DTS_GPIO_STATE_LCD_BIAS_ENP1);
 	MDELAY(2);
 	disp_dts_gpio_select_state(DTS_GPIO_STATE_LCD_BIAS_ENN1);
@@ -355,33 +350,63 @@ static void lcm_init(void)
 	data = 0x13;
 
 	ret = tps65132_write_bytes(cmd, data);
+
+	if (ret < 0)
+		no_printk("[LCM]icnl9911c--tps6132--cmd=%0x--i2c write error--\n",
+				cmd);
+	else
+		no_printk("[LCM]icnl9911c--tps6132--cmd=%0x--i2c write success--\n",
+				cmd);
+
 	cmd = 0x01;
 	data = 0x13;
 
 	ret = tps65132_write_bytes(cmd, data);
-	MDELAY(4);
+
+	if (ret < 0)
+		no_printk("[LCM]icnl9911c--tps6132--cmd=%0x--i2c write error--\n",
+				cmd);
+	else
+		no_printk("[LCM]icnl9911c--tps6132--cmd=%0x--i2c write success--\n",
+				cmd);
+
+	MDELAY(2);
+	MDELAY(9);
 	disp_dts_gpio_select_state(DTS_GPIO_STATE_LCM_RST_OUT0);
-	
-	MDELAY(4);
+	MDELAY(1);
+	MDELAY(9);
 	disp_dts_gpio_select_state(DTS_GPIO_STATE_LCM_RST_OUT1);
-	
-	MDELAY(4);
+	MDELAY(1);
+	MDELAY(9);
 	disp_dts_gpio_select_state(DTS_GPIO_STATE_LCM_RST_OUT0);
-	
-	MDELAY(4);
+	MDELAY(1);
+	MDELAY(9);
 	disp_dts_gpio_select_state(DTS_GPIO_STATE_LCM_RST_OUT1);
-	
-	MDELAY(4);
-	push_table(init_setting,
-		sizeof(init_setting) / sizeof(struct LCM_setting_table), 1);
+	MDELAY(1);
+	MDELAY(9);
+
+	push_table(init_setting, sizeof(init_setting) /
+		sizeof(struct LCM_setting_table), 1);
 }
 
 static void lcm_suspend(void)
 {
-	no_printk("[LCM]%s\n",__func__);
+	no_printk("[LCM]%s,tpd_gesture_flag = %d\n",__func__,tpd_gesture_flag);
+#if defined(CONFIG_TOUCHSCREEN_COMMON)
+	if (tpd_gesture_flag){
+		push_table(lcm_suspend_gesture_setting,
+		sizeof(lcm_suspend_gesture_setting) / sizeof(struct LCM_setting_table),
+			1);
+	} else {
+		push_table(lcm_suspend_setting,
+		sizeof(lcm_suspend_setting) / sizeof(struct LCM_setting_table),
+			1);
+	}
+#else
 	push_table(lcm_suspend_setting,
 		sizeof(lcm_suspend_setting) / sizeof(struct LCM_setting_table),
 			1);
+#endif
 }
 
 static void lcm_resume(void)
@@ -425,8 +450,6 @@ static void lcm_update(unsigned int x, unsigned int y, unsigned int width,
 #endif
 }
 
-#define LCM_ID_NT35595 (0x95)
-
 static unsigned int lcm_compare_id(void)
 {
 	unsigned int id0 = 0;
@@ -437,7 +460,7 @@ static unsigned int lcm_compare_id(void)
 
 	SET_RESET_PIN(1);
 	SET_RESET_PIN(0);
-	
+	MDELAY(1);
 
 	SET_RESET_PIN(1);
 	MDELAY(20);
@@ -450,7 +473,7 @@ static unsigned int lcm_compare_id(void)
 	id1 = buffer[1];     /* we only need ID */
 	id2 = buffer[2];     /* we only need ID */
 
-	no_printk("[LCM]%s,nt36525b id0 = 0x%x,id1 = 0x%x, id2 = 0x%x\n",
+	no_printk("[LCM]%s,icnl9911c id0 = 0x%x,id1 = 0x%x, id2 = 0x%x\n",
 		 __func__, id0,id1,id2);
 	if(id0 == 0x00 && id1 == 0x80 && id2 == 0x00)
 		return 1;
@@ -473,7 +496,7 @@ static unsigned int lcm_esd_check(void)
 	read_reg_v2(0x0A, buffer, 1);
 
 	if (buffer[0] != 0x9C) {
-		no_printk("[LCM][LCM ERROR] [0x0A]=0x%02x\n", buffer[0]);
+		pr_no_warn("[LCM][LCM ERROR] [0x0A]=0x%02x\n", buffer[0]);
 		return TRUE;
 	}
 	no_printk("[LCM][LCM NORMAL] [0x0A]=0x%02x\n", buffer[0]);
@@ -539,12 +562,13 @@ static unsigned int lcm_ata_check(unsigned char *buffer)
 static void lcm_setbacklight_cmdq(void *handle, unsigned int level)
 {
 
+	no_printk("[LCM]%s,icnl9911c backlight: level = %d lcd_bl_en = %d\n", __func__, level, lcd_bl_en);
 	if((0 != level) && (level <= 14))
 		level = 14;
 	level = level*72/100;
 #ifdef CONFIG_BACKLIGHT_SUPPORT_2047_FEATURE
-	bl_level[0].para_list[0] = level >> 8;
-	bl_level[0].para_list[1] = level & 0xFF;
+	bl_level[0].para_list[0] = level >> 3;
+	bl_level[0].para_list[1] = (level & 0b111) << 1;
 #else
 	bl_level[0].para_list[0] = level;
 #endif
@@ -585,8 +609,8 @@ static void *lcm_switch_mode(int mode)
 }
 
 
-struct LCM_DRIVER nt36525b_vdo_hdp_boe_dijing_lcm_drv = {
-	.name = "nt36525b_vdo_hdp_boe_dijing_drv",
+struct LCM_DRIVER icnl9911c_vdo_hdp_boe_tianma_lcm_drv = {
+	.name = "icnl9911c_vdo_hdp_boe_tianma_drv",
 	.set_util_funcs = lcm_set_util_funcs,
 	.get_params = lcm_get_params,
 	.init = lcm_init,

Dhamararf/android_kernel_mediatek_mt6765g

drivers/misc/mediatek/lcm/icnl9911c_vdo_hdp_boe_tianma/icnl9911c_vdo_hdp_boe_tianma.c
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2015 MediaTek Inc.
  * Copyright (C) 2021 XiaoMi, Inc.
+ * Copyright (C) 2022 TheKit
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -107,6 +108,7 @@ static const unsigned char LCD_MODULE_ID = 0x01;
 #define REGFLAG_RESET_HIGH	0xFFFF
 
 static struct LCM_DSI_MODE_SWITCH_CMD lcm_switch_mode_cmd;
+extern char *saved_command_line;
 
 #ifndef TRUE
 #define TRUE 1
@@ -116,6 +118,9 @@ static struct LCM_DSI_MODE_SWITCH_CMD lcm_switch_mode_cmd;
 #define FALSE 0
 #endif
 
+#if defined(CONFIG_TOUCHSCREEN_COMMON)
+extern bool tpd_gesture_flag;
+#endif
 struct LCM_setting_table {
 	unsigned int cmd;
 	unsigned char count;
@@ -123,52 +128,44 @@ struct LCM_setting_table {
 };
 
 static struct LCM_setting_table lcm_suspend_setting[] = {
-	{0x28, 0, {} },
-	{REGFLAG_DELAY, 2, {} },
-	{0x10, 0, {} },
-	{REGFLAG_DELAY, 145, {} },
+	{0xF0, 2, {0x5A, 0x59}},
+	{0xF1, 2, {0xA5, 0xA6}},
+	{0x26, 1, {0x08}},
+	{REGFLAG_DELAY, 1, {}},
+	{0x28, 1, {}},
+	{REGFLAG_DELAY, 20, {}},
+	{0x10, 1, {}},
+	{REGFLAG_DELAY, 110, {}},
+	{0xCC, 1, {0x01}},
+	{REGFLAG_DELAY, 10, {}},
+	{REGFLAG_END_OF_TABLE, 0, {}},
 };
 
+#if defined(CONFIG_TOUCHSCREEN_COMMON)
+static struct LCM_setting_table lcm_suspend_gesture_setting[] = {
+	{0x26, 1, {0x08}},
+	{REGFLAG_DELAY, 1, {}},
+	{0x28, 0, {}},
+	{REGFLAG_DELAY, 2, {}},
+	{0x10, 0, {}},
+	{REGFLAG_DELAY, 145, {}},
+	{REGFLAG_END_OF_TABLE, 0, {}},
+};
+#endif
+
 static struct LCM_setting_table init_setting[] = {
-#ifdef CONFIG_BACKLIGHT_SUPPORT_2047_FEATURE
-	{0xFF,0x01,{0x23}},
-	{REGFLAG_DELAY,1,{}},
-	{0xFB,0x01,{0x01}},
-	{0x00,0x01,{0x68}},//11bit
-	//PWM frequency 30K start
-	{0x07,0x01,{0x00}},
-	{0x08,0x01,{0x01}},
-	{0x09,0x01,{0x00}},
-	//PWM frequency 30K end
-#else
-	{0xFF,0x01,{0x23}},
-	{REGFLAG_DELAY,1,{}},
-	{0xFB,0x01,{0x01}},
-	{0x07,0x01,{0x20}},
-	{0x08,0x01,{0x07}},
-	{0x09,0x01,{0x02}},
-#endif
-
-	{0xFF,0x01,{0x10}},
-	{0xFB,0x01,{0x01}},
-	//Set_tear_on
-	{0x35,0x01,{0x00}},
-#ifdef CONFIG_BACKLIGHT_SUPPORT_2047_FEATURE
-	{0x51,0x02,{0x00,0x00}},
-#else
-	{0x51,0x01,{0x00}},
-#endif
-	{0x53,1,{0x2C}},
-	{0x55,1,{0x00}},
-	{0x68,2,{0x02,0x01}},
-	//Display_on
-	{0x29,0,{}},
-	//Sleep_out
-	{0x11,0,{}},
-	{REGFLAG_DELAY,100,{}},
-
-
-	/* {0x51,1,{0xFF}},     //      write   display brightness */
+	{0xF0, 2, {0x5A, 0x59}},
+	{0xF1, 2, {0xA5, 0xA6}},
+	{0xC1, 19, {0xC0, 0x0C, 0x20, 0x7C, 0x04, 0x50, 0x50, 0x04, 0x2A, 0x40, 0x36, 0x00, 0x07, 0xCF, 0xFF, 0xFF, 0x9E, 0x01, 0xC0}},
+	{0xE0, 13, {0x30, 0x00, 0x20, 0x18, 0x01, 0x1F, 0x23, 0x62, 0xDF, 0xA0, 0x04, 0xCC, 0x01}},
+	{0x35, 1, {0x00}},
+	{0x51, 2, {0x00, 0x00}},
+	{0x53, 1, {0x24}},
+	{0x11, 0, {}},
+	{REGFLAG_DELAY, 100, {}},
+	{0x29, 0, {}},
+	{REGFLAG_DELAY, 100, {}},
+	{0x26, 1, {0x01}},
 };
 
 #if 0
@@ -202,11 +199,7 @@ static struct LCM_setting_table lcm_deep_sleep_mode_in_setting[] = {
 };
 #endif
 static struct LCM_setting_table bl_level[] = {
-#ifdef CONFIG_BACKLIGHT_SUPPORT_2047_FEATURE
-	{0x51,2,{0x05,0xc2}},
-#else
-	{0x51,1,{0xB8}},
-#endif
+	{0x51, 2, {0xC2, 0x05}},
 	{REGFLAG_END_OF_TABLE, 0x00, {} }
 };
 
@@ -290,14 +283,14 @@ static void lcm_get_params(struct LCM_PARAMS *params)
 
 	params->dsi.PS = LCM_PACKED_PS_24BIT_RGB888;
 
-	params->dsi.vertical_sync_active = 2;
-	params->dsi.vertical_backporch = 8;
-	params->dsi.vertical_frontporch = 10;
+	params->dsi.vertical_sync_active = 4;
+	params->dsi.vertical_backporch = 18;
+	params->dsi.vertical_frontporch = 32;
 	params->dsi.vertical_active_line = FRAME_HEIGHT;
 
 	params->dsi.horizontal_sync_active = 4;
-	params->dsi.horizontal_backporch = 85;
-	params->dsi.horizontal_frontporch = 85;
+	params->dsi.horizontal_backporch = 80;
+	params->dsi.horizontal_frontporch = 80;
 	params->dsi.horizontal_active_pixel = FRAME_WIDTH;
 	params->dsi.ssc_disable = 1;
 #ifndef CONFIG_FPGA_EARLY_PORTING
@@ -315,7 +308,6 @@ static void lcm_get_params(struct LCM_PARAMS *params)
 	params->dsi.lcm_esd_check_table[0].cmd = 0x0A;
 	params->dsi.lcm_esd_check_table[0].count = 1;
 	params->dsi.lcm_esd_check_table[0].para_list[0] = 0x9C;
-
 }
 
 static void lcm_init_power(void)
@@ -324,10 +316,13 @@ static void lcm_init_power(void)
 	lcd_bl_en = 1;
 }
 
-
 static void lcm_suspend_power(void)
 {
 	no_printk("[LCM]%s\n",__func__);
+
+	if (tpd_gesture_flag) {
+		return;
+	}
 
 	disp_dts_gpio_select_state(DTS_GPIO_STATE_LCD_BIAS_ENN0);
 	MDELAY(2);
@@ -338,9 +333,6 @@ static void lcm_suspend_power(void)
 static void lcm_resume_power(void)
 {
 	no_printk("[LCM]%s\n",__func__);
-	disp_dts_gpio_select_state(DTS_GPIO_STATE_LCD_BIAS_ENP1);
-	
-	disp_dts_gpio_select_state(DTS_GPIO_STATE_LCD_BIAS_ENN1);
 }
 
 static void lcm_init(void)
@@ -348,6 +340,9 @@ static void lcm_init(void)
 	unsigned char cmd = 0x0;
 	unsigned char data = 0xFF;
 	int ret = 0;
+
+	no_printk("[LCM]%s\n",__func__);
+
 	disp_dts_gpio_select_state(DTS_GPIO_STATE_LCD_BIAS_ENP1);
 	MDELAY(2);
 	disp_dts_gpio_select_state(DTS_GPIO_STATE_LCD_BIAS_ENN1);
@@ -355,33 +350,63 @@ static void lcm_init(void)
 	data = 0x13;
 
 	ret = tps65132_write_bytes(cmd, data);
+
+	if (ret < 0)
+		no_printk("[LCM]icnl9911c--tps6132--cmd=%0x--i2c write error--\n",
+				cmd);
+	else
+		no_printk("[LCM]icnl9911c--tps6132--cmd=%0x--i2c write success--\n",
+				cmd);
+
 	cmd = 0x01;
 	data = 0x13;
 
 	ret = tps65132_write_bytes(cmd, data);
-	MDELAY(4);
+
+	if (ret < 0)
+		no_printk("[LCM]icnl9911c--tps6132--cmd=%0x--i2c write error--\n",
+				cmd);
+	else
+		no_printk("[LCM]icnl9911c--tps6132--cmd=%0x--i2c write success--\n",
+				cmd);
+
+	MDELAY(2);
+	MDELAY(9);
 	disp_dts_gpio_select_state(DTS_GPIO_STATE_LCM_RST_OUT0);
-	
-	MDELAY(4);
+	MDELAY(1);
+	MDELAY(9);
 	disp_dts_gpio_select_state(DTS_GPIO_STATE_LCM_RST_OUT1);
-	
-	MDELAY(4);
+	MDELAY(1);
+	MDELAY(9);
 	disp_dts_gpio_select_state(DTS_GPIO_STATE_LCM_RST_OUT0);
-	
-	MDELAY(4);
+	MDELAY(1);
+	MDELAY(9);
 	disp_dts_gpio_select_state(DTS_GPIO_STATE_LCM_RST_OUT1);
-	
-	MDELAY(4);
-	push_table(init_setting,
-		sizeof(init_setting) / sizeof(struct LCM_setting_table), 1);
+	MDELAY(1);
+	MDELAY(9);
+
+	push_table(init_setting, sizeof(init_setting) /
+		sizeof(struct LCM_setting_table), 1);
 }
 
 static void lcm_suspend(void)
 {
-	no_printk("[LCM]%s\n",__func__);
+	no_printk("[LCM]%s,tpd_gesture_flag = %d\n",__func__,tpd_gesture_flag);
+#if defined(CONFIG_TOUCHSCREEN_COMMON)
+	if (tpd_gesture_flag){
+		push_table(lcm_suspend_gesture_setting,
+		sizeof(lcm_suspend_gesture_setting) / sizeof(struct LCM_setting_table),
+			1);
+	} else {
+		push_table(lcm_suspend_setting,
+		sizeof(lcm_suspend_setting) / sizeof(struct LCM_setting_table),
+			1);
+	}
+#else
 	push_table(lcm_suspend_setting,
 		sizeof(lcm_suspend_setting) / sizeof(struct LCM_setting_table),
 			1);
+#endif
 }
 
 static void lcm_resume(void)
@@ -425,8 +450,6 @@ static void lcm_update(unsigned int x, unsigned int y, unsigned int width,
 #endif
 }
 
-#define LCM_ID_NT35595 (0x95)
-
 static unsigned int lcm_compare_id(void)
 {
 	unsigned int id0 = 0;
@@ -437,7 +460,7 @@ static unsigned int lcm_compare_id(void)
 
 	SET_RESET_PIN(1);
 	SET_RESET_PIN(0);
-	
+	MDELAY(1);
 
 	SET_RESET_PIN(1);
 	MDELAY(20);
@@ -450,7 +473,7 @@ static unsigned int lcm_compare_id(void)
 	id1 = buffer[1];     /* we only need ID */
 	id2 = buffer[2];     /* we only need ID */
 
-	no_printk("[LCM]%s,nt36525b id0 = 0x%x,id1 = 0x%x, id2 = 0x%x\n",
+	no_printk("[LCM]%s,icnl9911c id0 = 0x%x,id1 = 0x%x, id2 = 0x%x\n",
 		 __func__, id0,id1,id2);
 	if(id0 == 0x00 && id1 == 0x80 && id2 == 0x00)
 		return 1;
@@ -473,7 +496,7 @@ static unsigned int lcm_esd_check(void)
 	read_reg_v2(0x0A, buffer, 1);
 
 	if (buffer[0] != 0x9C) {
-		no_printk("[LCM][LCM ERROR] [0x0A]=0x%02x\n", buffer[0]);
+		pr_warn("[LCM][LCM ERROR] [0x0A]=0x%02x\n", buffer[0]);
 		return TRUE;
 	}
 	no_printk("[LCM][LCM NORMAL] [0x0A]=0x%02x\n", buffer[0]);
@@ -539,12 +562,13 @@ static unsigned int lcm_ata_check(unsigned char *buffer)
 static void lcm_setbacklight_cmdq(void *handle, unsigned int level)
 {
 
+	no_printk("[LCM]%s,icnl9911c backlight: level = %d lcd_bl_en = %d\n", __func__, level, lcd_bl_en);
 	if((0 != level) && (level <= 14))
 		level = 14;
 	level = level*72/100;
 #ifdef CONFIG_BACKLIGHT_SUPPORT_2047_FEATURE
-	bl_level[0].para_list[0] = level >> 8;
-	bl_level[0].para_list[1] = level & 0xFF;
+	bl_level[0].para_list[0] = level >> 3;
+	bl_level[0].para_list[1] = (level & 0b111) << 1;
 #else
 	bl_level[0].para_list[0] = level;
 #endif
@@ -585,8 +609,8 @@ static void *lcm_switch_mode(int mode)
 }
 
 
-struct LCM_DRIVER nt36525b_vdo_hdp_boe_dijing_lcm_drv = {
-	.name = "nt36525b_vdo_hdp_boe_dijing_drv",
+struct LCM_DRIVER icnl9911c_vdo_hdp_boe_tianma_lcm_drv = {
+	.name = "icnl9911c_vdo_hdp_boe_tianma_drv",
 	.set_util_funcs = lcm_set_util_funcs,
 	.get_params = lcm_get_params,
 	.init = lcm_init,